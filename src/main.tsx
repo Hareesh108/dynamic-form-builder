9d16d8a1a0f282def76638fbd99ff7689e3bb1db
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-16 15:43:51 +0530

    Added React Router config

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Suspense>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </StrictMode>
+);
